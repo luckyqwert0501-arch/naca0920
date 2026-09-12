31d8bbdb58ebc46217479c823aef5b61d5ee6375
luckyqwert0501-arch <luckyqwert0501@gmail.com>
2026-09-12 11:53:54 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // 這裡是你部署的 Google Apps Script 網頁應用程式網址。
 // 如果之後重新部署、網址改變了，只需要更新這一行即可。
-const NC_GAS_URL = "https://script.google.com/macros/s/AKfycbxQBLlwAS4SdBhigat0zLQz9gJLtanrmlxctx9beiz6J7463C7naSqCTdHeHiqycUkY/exec";
+const NC_GAS_URL = "https://script.google.com/macros/s/AKfycbyiuIO7xUD_yL9l_QZPReE3MT0EWAu75aWCjF5wGxa4snK9D9gI0n4wzZ3fJ_b2ps38/exec";
